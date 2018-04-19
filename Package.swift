b0e5b0af88b6b4207213b093bddf0b58d8a052a8
Jacob Williams <ponyboy47@gmail.com>
2018-04-19 14:39:00 -0600

    First commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CLibAVCodec",
+    pkgConfig: "libavcodec-ffmpeg",
+    providers: [
+        .apt(["libavcodec-dev"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Ponyboy47/CLibAVUtil.git", from: "0.1.0"),
+        .package(url: "https://github.com/Ponyboy47/CLibSWResample.git", from: "0.1.0")
+    ],
+)
---
+++ README.md
@@ -0,0 +1,13 @@
+# CLibAVCodec
+
+Swift C Bindings for the FFmpeg 
+
+## Installation
+
+Put this in your Package.swift as a dependency:
+```swift
+.package(url: "https://github.com/Ponyboy47/CLibAVCodec.git", from: "0.1.0")
+```
+
+## License
+MIT
